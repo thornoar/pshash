ad66d1b74aa28e741a05615844776481e105dbfb
Roman Maksimovich <r.a.maksimovich@gmail.com>
2024-12-31 17:20:09 +0100

    +default.nix

---
+++ default.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {}, ... }: import ./build/dynamic.nix { inherit pkgs; pname = "pshash"; }
+
+# { pkgs ? import <nixpkgs> {} }:
+# pkgs.stdenv.mkDerivation {
+#     pname = "pshash";
+#     version = "0.1.9.1";
+#     src = ./app;
+#     buildInputs = [
+#         pkgs.ghc
+#     ];
+#     buildPhase = ''
+#         ghc Main.hs -no-keep-hi-files -no-keep-o-files -o pshash
+#     '';
+#     installPhase = ''
+#         mkdir -p $out/bin
+#         mv pshash $out/bin/
+#     '';
+# }
